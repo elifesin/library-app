3ceee27a9da3532290693561c1e192e78e3583ab
Elif Esin <elfesin00@gmail.com>
2026-07-22 11:33:02 +0300

    Authors için Create-Read özellikleri eklendi

--- LibraryApp/LibraryApp/Controllers/AuthorController.cs
+++ LibraryApp/LibraryApp/Controllers/AuthorController.cs
@@ -1,13 +1,135 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using LibraryApp.Models;
 
 namespace LibraryApp.Controllers;
 
 public class AuthorController : Controller
 {
-    // GET
+    private readonly string _connectionString;
+
+    public AuthorController(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+    }
+    
+    // 
+    [HttpGet]
     public IActionResult Index()
     {
         
+        List<Authors> authorsList = new List<Authors>();
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string sqlQuery = "SELECT * FROM Authors";
+
+            using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Authors author = new Authors();
+                        author.Id = Convert.ToInt32(reader["Id"]);
+                        author.FirstName = reader["FirstName"].ToString()!;
+                        author.LastName = reader["LastName"].ToString()!;
+                        
+                        authorsList.Add(author);
+                    }
+                }
+            }
+        }
+        
+        return View(authorsList);
+    }
+
+    public IActionResult Create()
+    {
         return View();
     }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Authors author)
+    {
+        if (ModelState.IsValid)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string sqlQuery = "INSERT INTO Authors(FirstName, LastName) VALUES (@FirstName, @LastName)";
+                
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", author.FirstName);
+                    command.Parameters.AddWithValue("@LastName", author.LastName);
+
+                    connection.Open();
+                    command.ExecuteNonQuery(); 
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        
+        return View(author);
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        Authors author = new Authors();
+
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string sqlQuery = "SELECT * FROM Authors WHERE Id = @Id";
+
+            using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Id", id);
+                connection.Open();
+                
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        author.Id = Convert.ToInt32(reader["Id"]);
+                        author.FirstName = reader["FirstName"].ToString()!;
+                        author.LastName = reader["LastName"].ToString()!;
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+        }
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Authors author)
+    {
+        if (ModelState.IsValid)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string sqlQuery = "UPDATE Authors SET FirstName = @FirstName, LastName = @Lastname WHERE Id = @Id";
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@FirstName", author.FirstName);
+                    command.Parameters.AddWithValue("@LastName", author.LastName);
+                    
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(author);
+    }
+    
+    
 }
--- LibraryApp/LibraryApp/Program.cs
+++ LibraryApp/LibraryApp/Program.cs
@@ -1,52 +1,23 @@
-using Microsoft.EntityFrameworkCore;
-using LibaryApp.Data;
+using Microsoft.Data.SqlClient; // Raw SQL için gerekli kütüphane
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); 
 
-// --- TÜM SERVİSLER BURADA EKLENMELİ (BUILD'DEN ÖNCE) ---
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(); // MVC sistemi aktif edilir.
 
-builder.Services.AddDbContext<LibraryDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// -----------------------------------------------------
-
-// Servis kayıtları bittikten sonra uygulama inşa edilir:
-var app = builder.Build(); 
-
-// --- BUILD İŞLEMİNDEN SONRA SADECE MIDDLEWARE'LER YAZILIR ---
+var app = builder.Build(); // Uygulamayı oluştur.
+    
+// Standart Middleware ayarları
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 app.UseRouting();
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<LibraryDbContext>();
-        bool canConnect = context.Database.CanConnect();
-        
-        if (canConnect)
-        {
-            Console.WriteLine("--> HARİKA! Veritabanına başarıyla bağlanıldı.");
-        }
-        else
-        {
-            Console.WriteLine("--> HATA: Veritabanına ulaşılamıyor. Bilgileri kontrol et.");
-        }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"--> BAĞLANTI HATASI DETAYI: {ex.Message}");
-    }
-}
 
 app.Run(); 
